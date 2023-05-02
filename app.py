e926ea18f32f3131295a05eb3a8d6562b97bb68e
tykim6 <tylerkim@mit.edu>
2023-05-01 23:44:06 -0400

    add filtering on states and update 10 and 5

--- app.py
+++ app.py
@@ -30,7 +30,7 @@ Update: I have filtered out all states that Verijet does not service.
 )
 
 percentile = st.selectbox(
-    "Which top percentile of income would you like to see?", ("1%", "5%")
+    "Which top percentile of income would you like to see?", ("1%", "5%", "10%")
 )
 
 st.write("Currently Viewing Top ", percentile)
@@ -42,22 +42,35 @@ elif percentile == "5%":
 elif percentile == "10%":
     df = pd.read_csv("10p_zips.csv")
 
+# Filter options
+state_list = df["state"].unique().tolist()
+selected_state = st.multiselect("Filter by state:", state_list)
 
-# Define a layer to display on a map
-layer = pdk.Layer(
-    "ScatterplotLayer",
-    df,
-    pickable=True,
-    opacity=0.8,
-    stroked=True,
-    filled=True,
-    radius_scale=1,
-    radius_min_pixels=5,
-    line_width_min_pixels=1,
-    get_position=["longitude", "latitude"],
-    get_fill_color=[255, 140, 0],
-    get_line_color=[0, 0, 0],
-)
+df = df.dropna()
+# Filter the dataframe based on user input
+filtered_df = df.copy()
+
+if selected_state:
+    filtered_df = filtered_df[filtered_df["state"].isin(selected_state)]
+
+
+if selected_state:
+    # Define a layer to display on a map
+    layer = pdk.Layer(
+        "ScatterplotLayer",
+        filtered_df,
+        pickable=True,
+        opacity=0.8,
+        stroked=True,
+        filled=True,
+        radius_scale=1,
+        radius_min_pixels=5,
+        line_width_min_pixels=1,
+        get_position=["longitude", "latitude"],
+        get_fill_color=[255, 140, 0],
+        get_line_color=[0, 0, 0],
+    )
+
 
 st.pydeck_chart(
     pdk.Deck(
@@ -68,3 +81,7 @@ st.pydeck_chart(
         },
     )
 )
+
+
+# Display the filtered dataframe
+st.dataframe(filtered_df)
