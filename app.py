2ceb71736115e65a502377856f2ae9e30da7bf17
tykim6 <tylerkim@mit.edu>
2023-05-01 23:45:24 -0400

    add fnlity for no selected states

--- app.py
+++ app.py
@@ -71,6 +71,22 @@ if selected_state:
         get_line_color=[0, 0, 0],
     )
 
+else:
+    # Define a layer to display on a map
+    layer = pdk.Layer(
+        "ScatterplotLayer",
+        df,
+        pickable=True,
+        opacity=0.8,
+        stroked=True,
+        filled=True,
+        radius_scale=1,
+        radius_min_pixels=5,
+        line_width_min_pixels=1,
+        get_position=["longitude", "latitude"],
+        get_fill_color=[255, 140, 0],
+        get_line_color=[0, 0, 0],
+    )
 
 st.pydeck_chart(
     pdk.Deck(
